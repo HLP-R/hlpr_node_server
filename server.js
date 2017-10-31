7113006cfd473fbc02bf37c5de70132eeb3d114a
Siddhartha Banerjee <banerjs.sid@gmail.com>
2017-10-31 00:02:04 -0400

    Finally, a functioning server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
 
 // Debug
 var debug = require('debug')('server');
@@ -13,17 +14,53 @@ var app = express();
 
 // Setup the middleware as required
 app.use(compression());
-app.use(favicon(path.join(__dirname + 'static', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
-// TODO: Parse out the static file dirs
+// TODO: Parse out the static file dirs in the apps
 
-// Setup the route handlers
-app.use('/', blog); // FIXME
+// Setup the route handlers from the apps
+app.use('/teleop', require('./apps/hlpr_web_teleop/routes'));
 
-// TODO: Get the ws handlers
+// TODO: Get the ws handlers from the apps
 
 // TODO: Combine the http and ws handlers together
 
 // Serve the combined server
+var port = process.env.PORT || 8000;
+var address = process.env.IP_ADDR || '0.0.0.0';
+app.set('port', port);
+app.set('address', address);
+
+var server = http.createServer(app);
+server.listen(port, address);
+
+server.on('error', function(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+server.on('listening', function() {
+    var addr = server.address();
+    debug('Listening on ' + addr);
+});
