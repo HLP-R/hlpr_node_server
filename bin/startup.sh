5567a63b270569da8a23f736f5218dfa20208c54
RAIL <rail.robotics@gmail.com>
2017-11-01 20:05:56 -0400

    Lots of hackery to get the crontab to work. I'll debug this some other day

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # Startup the node server and store the logs in /tmp/hlpr_node_server.log
 
-set -eu
+set -e
+set -x
 set -o pipefail
 
-echo "Starting hlpr-node-server"
+echo "hlpr-node-server autostart script"
+
+# Do setup (such as setting env variables?)
+. /opt/ros/indigo/setup.sh
+. /home/vector/vector_ws/devel/setup.bash
+
+# Start the server
+echo "Starting node server"
 cd /home/vector/software/hlp_r/hlpr_node_server
-npm start
+/usr/local/bin/npm start > /tmp/hlpr_node_server.log 2>&1
